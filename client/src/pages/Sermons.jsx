a5a14aafedfbf571e91e765e57451d27c6702ffb
Amene Terhemen <ameneterh@gmail.com>
2024-02-07 21:25:16 +0100

    set templates for blog and sermon pages

--- client/src/pages/Sermons.jsx
+++ client/src/pages/Sermons.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Sermons() {
-  return <div className="min-h-screen">Sermons</div>;
+  return (
+    <div className="min-h-screen w-full max-w-6xl mx-auto mt-2 md:mt-10 p-3">
+      Sermons
+    </div>
+  );
 }
